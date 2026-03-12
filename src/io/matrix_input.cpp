2b301d0100e7c04ad2eb17472b150defd056e9d3
ArtemKostrichko <csc0kiee@gmail.com>
2026-03-12 22:00:29 +0300

    add MatrixInput skeleton

---
+++ src/io/matrix_input.cpp
@@ -0,0 +1,12 @@
+#include "io/matrix_input.h"
+
+#include "core/errors.h"
+
+namespace matrix::io {
+
+    matrix::core::Matrix MatrixInput::read(std::istream& in) const {
+        (void)in;
+        throw matrix::core::ParseError("MatrixInput::read is not implemented yet");
+    }
+
+} // namespace matrix::io
---
+++ src/io/matrix_input.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <istream>
+
+#include "core/matrix.h"
+
+namespace matrix::io {
+
+    class MatrixInput {
+    public:
+        [[nodiscard]] matrix::core::Matrix read(std::istream& in) const;
+    };
+
+} // namespace matrix::io
